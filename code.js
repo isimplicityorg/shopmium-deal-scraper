3233b24ac5aee721044595cc13f2ac8c7c2eb73f
Brian Caudill <brian.m.caudill@gmail.com>
2023-09-25 20:48:05 -0300

    Update code.js
    removed all the promise code and went with load / unload events of the popups
    reorganized the code so that it is hopefully easier to read and reuse.

--- code.js
+++ code.js
@@ -1,171 +1,186 @@
-// Define the maximum number of windows to open at once
-var maxWindowsOpen = 1;
-
-// Define the maximum number of cards to parse
-var maxCardsToParse = 5; // Change this value as needed
-
-// Track the number of open windows
-var openWindowCount = 0;
-
+var arrCouponUrls = getCouponUrls();
+var couponCountTotal = arrCouponUrls.length;
+var popupWindow = null
 // Create an array to store extracted data from each URL
 var extractedDataArray = [];
+var couponCount = 0
+var outputOptionDumpAllData = false;
+var outputOptionDbData = true;
+//parseFirstCoupon();
+console.log(arrCouponUrls)
+//console.log(arrCouponUrls[couponCount++])
+openPopup(arrCouponUrls[couponCount], "couponwindow" );
 
-// for now, you can only have one or the other options
-outputOptionDumpAllData = false;
-outputOptionDbData = true;
 
-// Function to parse the main page for coupon cards
-async function parseMainPageForCouponCards() {
-  const couponCards = document.querySelectorAll('.nodesListItem');
-  const couponURLs = Array.from(couponCards, card =>
-    card.querySelector('a.node').getAttribute('href')
-  );
-
-  // Use Promise.all to process the URLs
-  await Promise.all(
-    couponURLs.slice(0, maxCardsToParse).map(async couponURL => {
-      // Check if the maximum number of open windows is reached
-      if (openWindowCount >= maxWindowsOpen) {
-        // Wait for a window to close before opening a new one
-        await waitForOpenWindow();
-      }
-
-      const data = await openAndExtractData(couponURL);
-      console.log("Promise.all " + data);
-    })
-  );
-
-  // Create a single JSON file with all data
-  createJSONFile(extractedDataArray);
+function cleanText(text) {
+  // Remove non-printable characters and unwanted characters
+  if(text == "" || text == null)return;
+  text = removeNonUTF8Chars(text);
+  return text.replace(/[^ -~]+/g, '');
 }
 
-// Function to wait for an open window to close
-async function waitForOpenWindow() {
-  return new Promise(resolve => {
-    const checkInterval = setInterval(() => {
-      if (openWindowCount < maxWindowsOpen) {
-        clearInterval(checkInterval);
-        resolve();
-      }
-    }, 1000);
-  });
+function removeNonUTF8Chars(inputString) {
+  console.log(inputString )
+ 
+  // Use a regular expression to match only UTF-8 characters
+  var utf8Regex = /[^\x00-\x7F]+/g;
+  
+  // Replace all non-UTF-8 characters with an empty string
+  var cleanedString = inputString.replace(utf8Regex, '');
+  
+  return cleanedString;
 }
 
-// Function to open a URL in a new window and extract data
-async function openAndExtractData(couponURL) {
-  const popupWindow = window.open(couponURL, '_blank');
-  openWindowCount++;
-
-  try {
-    // Wait for the popup window's document to fully load
-    await waitForPopupLoad(popupWindow);
-
-    // Extract and return the data
-    const data = await extractDataFromWindow(popupWindow, couponURL);
-
-    // Wait for a certain condition (e.g., window closed) or a timeout
-    await waitForWindowClose(popupWindow);
-
-    // Close the popup window
-    popupWindow.close();
-    openWindowCount--;
-
-    return data;
-  } catch (error) {
-    console.error('Error in openAndExtractData:', error);
-    openWindowCount--; // Decrement the open window count on error
-    return null; // Handle the error gracefully
-  }
+function getNextCouponUrl(){
+  console.log(arrCouponUrls)
+    return arrCouponUrls[couponCount++];  //return arrCouponUrls[couponCount++]
+  //couponCount++;
 }
-
-// Rest of the code remains the same...
-
-
-// Function to wait for a window to close
-function waitForWindowClose(window) {
-  return new Promise(resolve => {
+function getCouponUrls(){
+    // Select all card elements with the class 'nodesListItem'
+    var couponCards = document.querySelectorAll('.nodesListItem');
     
-      if (window.closed) {
-        //clearInterval(checkInterval);
-        resolve();
-      }else{
-        //popupWindow.close();
-      }
+    // Optionally, set a limit on the number of cards to return (e.g., 5)
+    var limit = 10;
     
-  });
+    // Convert the NodeList into an array and slice it to limit the number of cards
+    if(limit == -1){
+      var couponArray = Array.from(couponCards);
+    }else{
+    var couponArray = Array.from(couponCards).slice(0, limit-1);
+    }
+    // Extract the URLs from the selected cards
+    var couponURLs = couponArray.map(card =>
+      card.querySelector('a.node').getAttribute('href')
+    );
+    
+    return couponURLs;
 }
 
-// Function to extract data from a window
-async function extractDataFromWindow(window, couponURL) {
-  return await extractAndLogInfo(window, couponURL); // Pass couponURL as an argument
-}
-
-// Function to extract data and log it
-async function extractAndLogInfo(popupWindow, couponURL) {
-  // Extract data from the new window's document
-  var rebateElement = popupWindow.document.querySelector('.Offers-RebateLine.Offers-RebateLine--highlight');
-  if (rebateElement) {
-    // Extract the content of the rebate element
-    var rebateAmount = rebateElement.innerText.trim();
+function openPopup(url, windowName) {
+  if(url==null || url=="")return;
+     popupWindow = window.open(url, windowName);
+  console.log("new popup " + windowName)
+  console.log(url)
+    if (popupWindow === null || typeof popupWindow === 'undefined') {
+      alert('Popup blocked by the browser. Please enable popups for this website.');
+    } else {
+      // Add an onload event handler directly in the popup window
+      popupWindow.onload = function () {
+        console.log('Popup has finished loading.');
+        onPopupLoad(popupWindow);
+        
+        // You can add additional actions here when the popup finishes loading
+      };
+    }
   }
-  var brandNameElement = popupWindow.document.querySelector('.heading-block-title');
-  var offerTitleElement = popupWindow.document.querySelector('.details-informations-title');
-  var offerTermsElement = popupWindow.document.querySelector('.Offers-DetailsBlock__content p');
-  var offerEndTimeElement = popupWindow.document.querySelector('.node_status__offer_details.time.offer-end');
-  var descriptionElement = popupWindow.document.querySelector('.heading-block-description.current');
-  var buyElement = popupWindow.document.querySelector('.Offers-RebateLine.Offers-RebateLine--after');
-
-  // Check if elements exist and extract their content
-  var brandName = brandNameElement ? brandNameElement.textContent.trim() : '';
-  var offerTitle = offerTitleElement ? offerTitleElement.textContent.trim() : '';
-  var offerTerms = offerTermsElement ? offerTermsElement.textContent.trim() : '';
-  var offerEndTime = offerEndTimeElement ? offerEndTimeElement.textContent.trim() : '';
-  var description = descriptionElement ? descriptionElement.textContent.trim() : '';
-  var buyInfo = buyElement ? buyElement.innerText.trim() : '';
-  var restrictions = getRestrictionsPopup(popupWindow.document);
-
-  if(outputOptionDumpAllData == true){
-  var couponData = {
-    rebateAmount,
-    brandName,
-    offerTitle,
-    offerTerms,
-    offerEndTime,
-    description,
-    buyInfo,
-    restrictions,
-    couponURL // Include the couponURL in the data
-  };
+    
+  function onPopupLoad(popupWindow){
+    //do something when a popup opens
+    	  console.log('onPopupLoad.');
+      extractDataFromPopup(popupWindow,arrCouponUrls[couponCount]);
   }
-  if(outputOptionDbData == true)
-    {
-couponData = {
-      cashBack: rebateAmount,
-      offerName: offerTitle,
-      offerDetails: description,
-      expiration: offerEndTime,
-      insertDate: "DIGITAL",
-      insertId: "SHOPMIUM",
-      url: couponURL,
-      categories: "",
-      source: "SHOPMIUM",
-      couponId: Math.random().toString(36).substring(7),
+  
+  function extractDataFromPopup(popupWindow, couponURL) {
+    // Extract data from the new window's document
+    var rebateElement = popupWindow.document.querySelector('.Offers-RebateLine.Offers-RebateLine--highlight');
+    if (rebateElement) {
+      // Extract the content of the rebate element
+      var rebateAmount = rebateElement.innerText.trim();
+    }
+    var brandNameElement = popupWindow.document.querySelector('.heading-block-title');
+    var offerTitleElement = popupWindow.document.querySelector('.details-informations-title');
+    var offerTermsElement = popupWindow.document.querySelector('.Offers-DetailsBlock__content p');
+    var offerEndTimeElement = popupWindow.document.querySelector('.node_status__offer_details.time.offer-end');
+    var descriptionElement = popupWindow.document.querySelector('.heading-block-description.current');
+    var buyElement = popupWindow.document.querySelector('.Offers-RebateLine.Offers-RebateLine--after');
+  
+    // Check if elements exist and extract their content
+    var brandName = brandNameElement ? cleanText(brandNameElement.textContent.trim()) : '';
+    var offerTitle = offerTitleElement ? cleanText(offerTitleElement.textContent.trim()) : '';
+    var offerTerms = offerTermsElement ? cleanText(offerTermsElement.textContent.trim() ): '';
+    var offerEndTime = offerEndTimeElement ? cleanText(offerEndTimeElement.textContent.trim()) : 'NO EXPIRATION';
+    var description = descriptionElement ? cleanText(descriptionElement.textContent.trim()) : '';
+    var buyInfo = buyElement ? cleanText(buyElement.innerText.trim()) : '';
+    var restrictions = cleanText(getRestrictionsPopup(popupWindow.document));
+  
+    if(outputOptionDumpAllData == true){
+    var couponData = {
+      rebateAmount,
+      brandName,
+      offerTitle,
+      offerTerms,
+      offerEndTime,
+      description,
+      buyInfo,
+      restrictions,
+      couponURL // Include the couponURL in the data
     };
     }
-  if(couponData)extractedDataArray.push(couponData);
-
-  // Save the data as JSON (you can modify this part to save data as needed)
-  var jsonData = JSON.stringify(couponData);
-
-  // Check if all URLs have been processed
-  if (extractedDataArray.length === maxWindowCount) {
-    // All URLs have been processed, create a single JSON file with all data
-    createJSONFile(extractedDataArray);
+    if(outputOptionDbData == true)
+      {
+  couponData = {
+        cashBack: rebateAmount,
+        offerName: description + " " + offerTitle ,
+        offerDetails: offerTitle,
+        expiration: offerEndTime,
+        insertDate: "DIGITAL",
+        insertId: "SHOPMIUM",
+        url: couponURL,
+        categories: "",
+        source: "SHOPMIUM",
+        couponId: Math.random().toString(36).substring(7),
+      };
+      }
+    if(couponData)extractedDataArray.push(couponData);
+  
+    // Save the data as JSON (you can modify this part to save data as needed)
+    //var jsonData = JSON.stringify(couponData);
+    // After data extraction is complete, call the callback function
+    //return extractedDataArray;
+    console.log(extractedDataArray)
+    console.log("closed")
+    closePopup();
   }
+  
+  
+  function closePopup() {
+    if (popupWindow && !popupWindow.closed) {
+      popupWindow.close();
+       onPopupUnLoad();
+      //do something when a popup closes
+    }
+  }
+  
+  function onPopupUnLoad(){
+    //do something when a popup closes
+    //i dont think there is anything to do here
+   console.log("popup closed couponCount " + couponCount)
+    //if this is the last page then print the data to a file
+    if(arrCouponUrls[couponCount] ==  null || arrCouponUrls[couponCount] == ""){
+    createJSONFile(extractedDataArray);
+    }else{
+      openPopup(arrCouponUrls[couponCount++],"couponwindow");
+    }
+    }
+  
+  
+  function getRestrictionsPopup(doc){
+  // Assuming you have a reference to the popup element
+  var popupElement = doc.querySelector('.webPopup.warningPopup');
 
-  // After data extraction is complete, call the callback function
-  //return extractedDataArray;
-  popupWindow.close();
+  if (popupElement) {
+    // Find the message element within the popup
+    var messageElement = popupElement.querySelector('.webPopup-message p');
+
+    if (messageElement) {
+      // Extract the text content of the message element
+      var popupText = messageElement.textContent.trim();
+    
+      // Log or use the extracted text as needed
+      return popupText;
+    }
+  }
 }
 
 // Function to create a single JSON file with all data
@@ -188,23 +203,3 @@ console.log(jsonData)
   // Trigger a click event on the download link to initiate the download
   a.click();
 }
-
-function getRestrictionsPopup(doc){
-  // Assuming you have a reference to the popup element
-  var popupElement = doc.querySelector('.webPopup.warningPopup');
-
-  if (popupElement) {
-    // Find the message element within the popup
-    var messageElement = popupElement.querySelector('.webPopup-message p');
-
-    if (messageElement) {
-      // Extract the text content of the message element
-      var popupText = messageElement.textContent.trim();
-    
-      // Log or use the extracted text as needed
-      return popupText;
-    }
-  }
-}
-
-parseMainPageForCouponCards();
